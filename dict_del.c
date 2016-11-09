d9fbea7b40d39eda42f697dc712bed81b4aee1d4
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2016-11-09 04:52:00 +0000

    fix dict_del

--- dict_del.c
+++ dict_del.c
@@ -6,7 +6,7 @@
 /*   By: qle-guen <qle-guen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/09 02:29:29 by qle-guen          #+#    #+#             */
-/*   Updated: 2016/11/09 03:01:37 by qle-guen         ###   ########.fr       */
+/*   Updated: 2016/11/09 04:51:56 by qle-guen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ int			dict_del(t_dict *d, char *key)
 
 	if (!(ent = dict_find_ent(d, key)))
 		return (0);
-	free(ent->key);
 	ent->key = NULL;
 	ent->val.used = 0;
 	return (1);
