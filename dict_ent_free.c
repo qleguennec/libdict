8eeeba2cea4e3f152dd4827917543fc9ed6961c0
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2016-11-12 01:50:20 +0000

    fix leaks

--- dict_ent_free.c
+++ dict_ent_free.c
@@ -6,7 +6,7 @@
 /*   By: qle-guen <qle-guen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/10 19:21:02 by qle-guen          #+#    #+#             */
-/*   Updated: 2016/11/12 01:24:07 by qle-guen         ###   ########.fr       */
+/*   Updated: 2016/11/12 01:34:05 by qle-guen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,5 @@ void		dict_ent_free(t_dict *d, t_dict_ent *ent)
 	ent->val.total = 0;
 	ent->key = NULL;
 	free(ent->val.data);
+	ent->val.data = NULL;
 }
--- dict_free.c
+++ dict_free.c
@@ -6,7 +6,7 @@
 /*   By: qle-guen <qle-guen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/10 19:25:17 by qle-guen          #+#    #+#             */
-/*   Updated: 2016/11/11 15:13:03 by qle-guen         ###   ########.fr       */
+/*   Updated: 2016/11/12 01:33:27 by qle-guen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 void		dict_free(t_dict *d)
 {
-	dict_map(d, DICT_EXIST, &dict_ent_free);
+	dict_clear(d);
+	dict_map(d, DICT_DELETED, &dict_ent_free);
 	free(d->ents);
 	ft_lstfree(d->free);
 }
