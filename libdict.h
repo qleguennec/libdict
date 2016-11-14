f5e863338720d281510d307b632cca31728bb836
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2016-11-14 16:06:14 +0100

    fix norm

--- libdict.h
+++ libdict.h
@@ -6,7 +6,7 @@
 /*   By: qle-guen <qle-guen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/09 00:15:41 by qle-guen          #+#    #+#             */
-/*   Updated: 2016/11/12 21:59:56 by qle-guen         ###   ########.fr       */
+/*   Updated: 2016/11/14 16:06:06 by qle-guen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,20 +42,22 @@ typedef struct	s_dict
 	t_list		*free;
 }				t_dict;
 
-
 char			**dict_str_export(t_dict *d, char *sep);
 int				dict_del(t_dict *d, void *key);
 int				dict_istype(t_dict_ent *ent, int type);
 int				dict_iter(t_dict *d, t_dict_ent **ent, int type);
-int				dict_modify(t_dict *d, void *key, int type, void (*f)(t_dict *, t_dict_ent *));
-int				dict_str_import(t_dict *d, char *s, char *sep, void (*f) (t_dict *, void *, char *));
+int				dict_modify
+	(t_dict *d, void *key, int type, void (*f)(t_dict *, t_dict_ent *));
+int				dict_str_import
+	(t_dict *d, char *s, char *sep, void (*f) (t_dict *, void *, char *));
 long			dict_str_hash(char *s);
-size_t   		dict_size(t_dict *d, int type);
+size_t			dict_size(t_dict *d, int type);
 t_dict_ent		*dict_find_ent(t_dict *d, void *key, int type);
 t_dict_ent		*dict_find_match(t_dict *d, void *key, int type);
 t_dict_ent		*dict_find_maybe(t_dict *d, void *key, int type);
 t_dict_ent		*dict_lookup(t_dict *d, void *key);
-void			*dict_fmap(t_dict *d, int type, void *(*f)(t_dict *d, void *), void *ctxt);
+void			*dict_fmap
+	(t_dict *d, int type, void *(*f)(t_dict *d, void *), void *ctxt);
 void			dict_add(t_dict *d, void *key, void *val, size_t size);
 void			dict_clear(t_dict *d);
 void			dict_cpy(t_dict *dest, t_dict *src);
@@ -65,9 +67,12 @@ void			dict_ent_cpy(t_dict *d, t_dict_ent *new);
 void			dict_ent_del(t_dict *d, t_dict_ent *ent);
 void			dict_ent_free(t_dict *d, t_dict_ent *ent);
 void			dict_free(t_dict *d);
-void			dict_init(t_dict *d, size_t len, long (*hashf)(void *), t_cmp_f cmp_f);
-void			dict_map(t_dict *d, int type, void (*f)(t_dict *, t_dict_ent *));
-void			dict_map2(t_dict *dest, t_dict *src, int type, void (*f)(t_dict *d, t_dict_ent *ent));
+void			dict_init
+	(t_dict *d, size_t len, long (*hashf)(void *), t_cmp_f cmp_f);
+void			dict_map
+	(t_dict *d, int type, void (*f)(t_dict *, t_dict_ent *));
+void			dict_map2(t_dict *dest
+	, t_dict *src, int type, void (*f)(t_dict *d, t_dict_ent *ent));
 void			dict_print(t_dict *d, char *sep, char *nl);
 void			dict_regen(t_dict *d);
 void			dict_set(t_dict *d, void *key, void *val, size_t size);
