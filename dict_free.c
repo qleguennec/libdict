e5ed3aebd49fc3287e5ec5716be0c868eecc68fc
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2017-03-31 14:18:57 +0200

    fix norm

--- dict_free.c
+++ dict_free.c
@@ -6,7 +6,7 @@
 /*   By: qle-guen <qle-guen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/10 19:25:17 by qle-guen          #+#    #+#             */
-/*   Updated: 2017/03/09 11:58:00 by qle-guen         ###   ########.fr       */
+/*   Updated: 2017/03/31 14:18:30 by qle-guen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,9 @@ static void
 	}
 }
 
-void		dict_free(t_dict *d)
+void
+	dict_free
+	(t_dict *d)
 {
 	dict_clear(d);
 	dict_map(d, DICT_DELETED, &dict_ent_free);
